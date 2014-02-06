78dad93d205ac13e56c6dd59c1467cc18d73b6f0
Barry Mullan <mullan@gmail.com>
2014-02-05 22:02:15 -0500

    added app settings, improved sizing

--- App.js
+++ App.js
@@ -3,240 +3,262 @@ var app = null;
 var types = ["PortfolioItem/Feature","PortfolioItem/Initiative"];
 
 Ext.define('CustomApp', {
-    extend: 'Rally.app.App',
-    componentCls: 'app',
-    // items:{ html:'<a href="https://help.rallydev.com/apps/2.0rc2/doc/">App SDK 2.0rc2 Docs</a>'},
-    launch: function() {
-        //Write app code here
-        app = this;
+	extend: 'Rally.app.App',
+	settingsScope: 'app',
+	componentCls: 'app',
+	config: {
+		defaultSettings: {
+			rows: 'Initiative',
+			cols: 'InvestmentCategory'
+		}
+	},
+	// items:{ html:'<a href="https://help.rallydev.com/apps/2.0rc2/doc/">App SDK 2.0rc2 Docs</a>'},
+	launch: function() {
+		//Write app code here
+		app = this;
 
 		var panel = Ext.create('Ext.container.Container', {
-		            itemId : 'panel',
-		            title: 'Hello',
-		            width: 800,
-		            height: 600,
-		            html: '<p></p>'
-		        });
+					itemId : 'panel',
+					title: 'Hello',
+					// width: 800,
+					// height: 600,
+					html: '<p></p>'
+		});
 
-        this.add(panel);
-        var p = this.down("#panel");
-        app.jqPanel = "#"+p.id;
+		// read the saved settings.
+		app.settingsRows = app.getSetting('rows');
+		app.settingsCols = app.getSetting('cols');
 
-        app.mask = new Ext.LoadMask(Ext.getBody(), {msg:"Please wait..."});
-        app.mask.show();
-        app.loadProjects();
-    },
+		this.add(panel);
+		var p = this.down("#panel");
+		app.jqPanel = "#"+p.id;
 
-    loadProjects : function() {
+		app.mask = new Ext.LoadMask(Ext.getBody(), {msg:"Please wait..."});
+		app.mask.show();
+		app.loadProjects();
+	},
 
-    	var configs = [ {
+	getSettingsFields: function() {
+		return [
+			{
+				name: 'rows',
+				xtype: 'rallytextfield'
+			},
+			{
+				name: 'cols',
+				xtype: 'rallytextfield'
+			}
+		];
+	},
+
+	loadProjects : function() {
+
+		var configs = [ {
 			model : "Project",
-    	    fetch : ["Name","ObjectID"],
-        } ];
+			fetch : ["Name","ObjectID"]
+		} ];
 
-        async.map(configs,app.wsapiQuery,function(err,results) {
-        	app.projects = results[0];
-	        app.readPortfolioItems();
-        });
+		async.map(configs,app.wsapiQuery,function(err,results) {
+			app.projects = results[0];
+			app.readPortfolioItems();
+		});
 
-    },
+	},
 
-    createConfigForPortfolioType : function(type) {
+	createConfigForPortfolioType : function(type) {
 
-    	var projects = _.map(app.projects,function(p) { return p.get("ObjectID")});
+		var projects = _.map(app.projects,function(p) { return p.get("ObjectID");});
 
-	    return {
-	        fetch : ['Name','_UnformattedID','ObjectID','_TypeHierarchy','c_STO', '_ItemHierarchy',
-	        			'InvestmentCategory','PortfolioItemType','State','Owner','Project','Parent'
-	        		],
-	        hydrate : ['_TypeHierarchy','State','PortfolioItemType','InvestmentCategory'],
-	        pageSize:1000,
-	        find : {
-	            '_TypeHierarchy' : { "$in" : [type]} ,
-	            'Project' : { "$in": projects }, 
-	            __At : 'current'
-	        },
-	    }
-    },
+		return {
+			fetch : ['Name','_UnformattedID','ObjectID','_TypeHierarchy','c_STO', '_ItemHierarchy',
+						'InvestmentCategory','PortfolioItemType','State','Owner','Project','Parent'
+					],
+			hydrate : ['_TypeHierarchy','State','PortfolioItemType','InvestmentCategory'],
+			pageSize:1000,
+			find : {
+				'_TypeHierarchy' : { "$in" : [type]} ,
+				'Project' : { "$in": projects }, 
+				__At : 'current'
+			}
+		};
+	},
 
-    readPortfolioItems : function() {
+	readPortfolioItems : function() {
 
-    	var configs = _.map( types, function(type) {
-        	return app.createConfigForPortfolioType(type);
-        })
+		var configs = _.map( types, function(type) {
+			return app.createConfigForPortfolioType(type);
+		});
 
-        async.mapSeries( configs, app.readSnapshots, function(err,results) {
+		async.mapSeries( configs, app.readSnapshots, function(err,results) {
 
-       		app.addThemesToFeatures(results[0],results[1]);
+			app.addThemesToFeatures(results[0],results[1]);
 
-        });
-    },
+		});
+	},
 
-    addThemesToFeatures : function(features,initiatives) {
+	addThemesToFeatures : function(features,initiatives) {
 
-        console.log("initiatives",initiatives);
+		app.addOwners(features);
+		app.addTeamNames(features);
 
-        app.addOwners(features);
-        app.addTeamNames(features);
-
-        _.each(features, function(f){
-            var initiative = _.find( initiatives, function(i) { 
-                return f.get("Parent") === i.get("ObjectID");
-            });
-            f.set("Initiative", initiative  ? initiative.get("Name") : "None");
-            f.set("Foo", initiative  ? initiative.get("Name") : "None");
-            // console.log("i:",f.get("Initiative"),f.get("Initiative")==undefined,f.get("Initiative")==null);
-        });
+		_.each(features, function(f){
+			var initiative = _.find( initiatives, function(i) { 
+				return f.get("Parent") === i.get("ObjectID");
+			});
+			f.set("Initiative", initiative  ? initiative.get("Name") : "None");
+		});
 
 
-    },
+	},
 
-    cleanUpFieldNames : function(features) {
+	cleanUpFieldNames : function(features) {
 
-        _.each( features, function(f,i) {
-            var keys = _.keys(f);
-            if (i==0) console.log("keys",keys);
+		_.each( features, function(f,i) {
+			var keys = _.keys(f);
 
-            keys = _.filter(keys,function(k){
-                return k.substring(0,2) === "c_";
-            })
-            // if (i==0) console.log("keys",keys);
-            _.each(keys,function(k){
-                var newKey = k.substring(2);
-                f[newKey] = f[k];
-                delete f[k];
-            });
+			keys = _.filter(keys,function(k){
+				return k.substring(0,2) === "c_";
+			});
+			_.each(keys,function(k){
+				var newKey = k.substring(2);
+				f[newKey] = f[k];
+				delete f[k];
+			});
+		});
 
-        })
+		return features;
 
-        return features;
+	},
 
-    },
+	addTeamNames : function(features) {
 
-    addTeamNames : function(features) {
+		_.each( features, function(f) {
+			var p = _.find(app.projects,function(pr) { 
+				return pr.get("ObjectID")=== f.get("Project");
+			});
+			f.set("Team",p.get("Name"));
+		});
 
-    	_.each( features, function(f) {
-    		var p = _.find(app.projects,function(pr) { 
-    			return pr.get("ObjectID")=== f.get("Project");
-    		});
-    		f.set("Team",p.get("Name"));
-    	});
+		app.pivotTable(features);
 
-    	app.pivotTable(features);
+	},
 
-    },
+	addOwners : function(features) {
 
-    addOwners : function(features) {
+		var ownerIds = _.map(features,function(f) {
+			var o = f.get("Owner");
+			if (o !== undefined && o !== null)
+				return o;
+			else
+				return null;
+		});
+		ownerIds = _.compact(ownerIds);
+		ownerIds = _.uniq(ownerIds);
 
-    	var ownerIds = _.map(features,function(f) {
-    		var o = f.get("Owner");
-    		if (o !== undefined && o !== null)
-    			return o;
-    		else
-    			return null;
-    	});
-    	ownerIds = _.compact(ownerIds);
-    	ownerIds = _.uniq(ownerIds);
-    	console.log("distinct owners",ownerIds);
+		var configs = _.map(ownerIds, function(o) {
+			return { 
+				model : "User",
+				fetch : ["UserName","DisplayName"],
+				filters : [{property:"ObjectID",value:o}]
+			};
+		});
 
-    	var configs = _.map(ownerIds, function(o) {
-    		return { 
-    			model : "User",
-        	    fetch : ["UserName","DisplayName"],
-	            filters : [{property:"ObjectID",value:o}]
-            };
-    	});
-
-    	async.map(configs,app.wsapiQuery,function(err,results) {
-    		app.owners = _.pluck( results, function(r) { return r[0];});
-    		app.owners = _.compact(app.owners);
-    		console.log("owners",app.owners);
-    		_.each(features,function(f) {
-    			var ownerName = _.find(app.owners,function(o) {
-    				return (o.get("ObjectID") === f.get("Owner"));
-    			})
-    			if (ownerName !== undefined && ownerName !== null) 
-    				f.set("OwnerName",
-    					ownerName.get("DisplayName")!==null && ownerName.get("DisplayName").length > 0
-    						? ownerName.get("DisplayName") 
-    						: ownerName.get("UserName") );
-    			else
-    				f.set("OwnerName","");
-    		});
-	    	app.addTeamNames(features);
-    	});
-    },
+		async.map(configs,app.wsapiQuery,function(err,results) {
+			app.owners = _.pluck( results, function(r) { return r[0];});
+			app.owners = _.compact(app.owners);
+			_.each(features,function(f) {
+				var ownerName = _.find(app.owners,function(o) {
+					return (o.get("ObjectID") === f.get("Owner"));
+				});
+				if (ownerName !== undefined && ownerName !== null) 
+					f.set("OwnerName",
+						ownerName.get("DisplayName")!==null && ownerName.get("DisplayName").length > 0 ? ownerName.get("DisplayName") : ownerName.get("UserName") );
+				else
+					f.set("OwnerName","");
+			});
+			app.addTeamNames(features);
+		});
+	},
 
 
-    pivotTable : function(features) {
+	pivotTable : function(features) {
 
-    	var data = _.map(features,function(s) { 
-            return s.data;
-        });
+		var rows = app.settingsRows.split(",");
+		var cols = app.settingsCols.split(",");
+		console.log("rows:",rows,"cols:",cols);
 
-        data = app.cleanUpFieldNames(data);
+		var data = _.map(features,function(s) { 
+			return s.data;
+		});
 
-        var initDeriver = function(record) {
-            return record.Initiative;
-        };
+		data = app.cleanUpFieldNames(data);
 
-        var derived = {
-            "Initiative" : initDeriver
-        }   
-
-        console.log("data",data);
-
-        $(app.jqPanel).pivotUI(
-            data,                    
-            {
-                derivedAttributes : derived,
-                // aggregators : { cycleTime : cycleTime },
-                cols: ["InvestmentCategory"],
-                rows: ["Initiative"],
-                hiddenAttributes : ["Project","Owner","ObjectID","_TypeHierarchy","_UnformattedID","_ValidFrom","_ValidTo","PortfolioItemType","_ItemHierarchy"]
-            }
-        );
-
-        app.mask.hide();
-
-    },
+		var initDeriver = function(record) {
+			return record.Initiative;
+		};
+		var ownerDeriver = function(record) {
+			return record.OwnerName;
+		};
 
 
-    readSnapshots : function( config, callback) {
-        console.log("reading page of snapshots...");
-         var storeConfig = {
-            find : config.find,
-            autoLoad : true,
-            pageSize:1000,
-            limit: 'Infinity',
-            fetch: config.fetch,
-            hydrate: config.hydrate,
-            listeners : {
-                scope : this,
-                load: function(store, snapshots, success) {
-                    callback(null,snapshots);
-                }
-            }
-        };
-        var snapshotStore = Ext.create('Rally.data.lookback.SnapshotStore', storeConfig);
-    },
+		var derived = {
+			"Initiative" : initDeriver,
+			"OwnerName" : ownerDeriver
+		};   
 
-    wsapiQuery : function( config , callback ) {
-        Ext.create('Rally.data.WsapiDataStore', {
-            autoLoad : true,
-            limit : "Infinity",
-            model : config.model,
-            fetch : config.fetch,
-            filters : config.filters,
-            // context: config.context,
-            listeners : {
-                scope : this,
-                load : function(store, data) {
-                    callback(null,data);
-                }
-            }
-        });
-    }
+
+		$(app.jqPanel).pivotUI(
+			data,                    
+			{
+				derivedAttributes : derived,
+				// aggregators : { cycleTime : cycleTime },
+				// cols: ["InvestmentCategory"],
+				// rows: ["Initiative"],
+				cols: cols,
+				rows: rows,
+				hiddenAttributes : ["Project","Owner","ObjectID","_TypeHierarchy","_UnformattedID","_ValidFrom","_ValidTo","PortfolioItemType","_ItemHierarchy"]
+			}
+		);
+
+		app.mask.hide();
+
+	},
+
+	readSnapshots : function( config, callback) {
+		console.log("reading page of snapshots...");
+		var storeConfig = {
+			find : config.find,
+			autoLoad : true,
+			pageSize:1000,
+			limit: 'Infinity',
+			fetch: config.fetch,
+			hydrate: config.hydrate,
+			listeners : {
+				scope : this,
+				load: function(store, snapshots, success) {
+					callback(null,snapshots);
+				}
+			}
+		};
+		var snapshotStore = Ext.create('Rally.data.lookback.SnapshotStore', storeConfig);
+	},
+
+	wsapiQuery : function( config , callback ) {
+		Ext.create('Rally.data.WsapiDataStore', {
+			autoLoad : true,
+			limit : "Infinity",
+			model : config.model,
+			fetch : config.fetch,
+			filters : config.filters,
+			// context: config.context,
+			listeners : {
+				scope : this,
+				load : function(store, data) {
+					callback(null,data);
+				}
+			}
+		});
+	}
 
 });
 
